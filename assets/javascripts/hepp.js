a06997e897726f96991716181a9d95c640e53e8c
Jason Heppler <jason.heppler@gmail.com>
2022-10-17 09:17:12 -0500

    modifying footnotes

--- assets/javascripts/hepp.js
+++ assets/javascripts/hepp.js
@@ -1,5 +1,7 @@
 import accessibleFootnotes from "./accessibleFootnotes.js";
 import enableFloatingFootnotes from "./marginalia.js";
+const { littlefoot } = require('littlefoot')
 
-accessibleFootnotes();
-enableFloatingFootnotes();
+// accessibleFootnotes();
+// enableFloatingFootnotes();
+// littlefoot();
