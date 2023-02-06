685e624e8f3d85091ffe60ccf7dcb781b12f7ef0
Jason Heppler <jason.heppler@gmail.com>
2023-02-05 21:14:37 -0600

    Forgot to remove dark color testing; added hamburger menu.

--- assets/javascripts/hepp.js
+++ assets/javascripts/hepp.js
@@ -1,17 +1,50 @@
-import accessibleFootnotes from "./accessibleFootnotes.js";
-import enableFloatingFootnotes from "./marginalia.js";
+// import accessibleFootnotes from "./accessibleFootnotes.js";
+// import enableFloatingFootnotes from "./marginalia.js";
 const { littlefoot } = require('littlefoot')
 import Headroom from "headroom.js";
 
 // accessibleFootnotes();
 // enableFloatingFootnotes();
+
 littlefoot({
     anchorPattern: /fn:/,
 });
 
-// grab an element
+// Headroom
 const header = document.getElementById("header-container");
-// construct an instance of Headroom, passing the element
 const headroom  = new Headroom(header, {tolerance : 0,});
-// initialise
 headroom.init();
+
+// Hamburger menu 
+const menuItems = document.querySelectorAll(".menu")
+const hamburger= document.querySelector(".hamburger")
+
+function toggleMenu() {
+  if (hamburger.classList.contains("is-active")){
+    menuItems.forEach(
+      function(menuItem) { 
+        menuItem.classList.remove("mobile")
+      }
+    )
+    header.classList.remove("is-open")
+    headroom.init();
+    hamburger.classList.remove("is-active")
+  } else {
+    menuItems.forEach(
+      function(menuItem) { 
+        menuItem.classList.add("mobile")
+      }
+    )
+    header.classList.add("is-open")
+    headroom.destroy();
+    hamburger.classList.add("is-active")
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu)
+
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu)
+  }
+)
